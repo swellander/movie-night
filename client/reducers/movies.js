a6409ef954ff0076be5ba395cd566ea6e8c88926
Sam Wellander <samwellander@gmail.com>
2018-09-21 16:54:06 -0400

    add write movie into reducer

--- client/reducers/movies.js
+++ client/reducers/movies.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
 const initialState = {
-  list: []
+  list: [],
+  draft: ''
 }
 
 const LOAD_MOVIES = 'LOAD_MOVIES';
+const WRITE_MOVIE = 'WRITE_MOVIE';
 
 const loadMovies = movies => ({ type: LOAD_MOVIES, movies });
+export const writeMovie = draft => ({ type: WRITE_MOVIE, draft });
 
 export const _loadMovies = () => dispatch => {
   axios.get('/api/movies')
@@ -24,6 +27,8 @@ const movieReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_MOVIES:
       return { ...state, list: action.movies }
+    case WRITE_MOVIE:
+      return { ...state, draft: action.draft }
     default:
       return state;
   }
