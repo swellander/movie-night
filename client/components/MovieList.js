8f839e9e071e4271352f20b24f4d556b971b785a
Sam Wellander <samwellander@gmail.com>
2018-09-21 20:41:19 -0400

    add some styling

--- client/components/MovieList.js
+++ client/components/MovieList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Movie from './Movie';
 import { connect } from 'react-redux';
 import { _loadMovies } from '../reducers/movies';
+import Grid from '@material-ui/core/Grid';
 
 class MovieList extends Component {
   componentDidMount() {
@@ -10,13 +11,20 @@ class MovieList extends Component {
 
   render() {
     const styles = {
-      marginTop: '10vh'
+      marginTop: '10vh',
+      flexGrow: 1
     }
     return (
       <div style={styles}>
-        {this.props.movies.map(movie => {
-          return <Movie movie={movie} />
-        })}
+        <Grid container spacing={24}>
+          {this.props.movies.map(movie => {
+            return (
+              <Grid item justify="space-between" lg={3} md={6} xs={12}>
+                <Movie movie={movie} />
+              </Grid>
+            )
+          })}
+        </Grid>
       </div>
     )
   }
